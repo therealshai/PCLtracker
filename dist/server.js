bc4a8f766bfbc186033ae62bb836f5354c6f6894
Syed Shaista Tabassum <sshai4020@gmail.com>
2024-06-18 23:54:58 +0530

    Update .gitignore , initial files- Frontend,backend

---
+++ dist/server.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT || 5000;
+// Middleware
+app.use(express_1.default.json());
+// Define Routes
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+// Start Server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
