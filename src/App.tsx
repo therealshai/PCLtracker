aa54d6982e3fd9e34066573ff13e2ff516841d6f
Syed Shaista Tabassum <97975821+therealshai@users.noreply.github.com>
2024-06-19 20:27:20 +0530

    Added frontend files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500">
+      <div className='bg-green-500'> hi</div>
+    </div>
+  );
+}
+
+export default App;
