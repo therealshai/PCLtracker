c9b95b741d9e48336dcdcbee1c7cbf3e254e6cd6
Syed Shaista Tabassum <sshai4020@gmail.com>
2024-06-21 23:02:38 +0530

    Basic files init
    Added Toggle component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,10 +7,11 @@ function App() {
   return (
     <div className="flex items-center justify-center bg-background-blue-light h-screen rounded-md">
       <div className='w-4/12 h-4/12 items-center flex-row bg-background-blue'>
-         <div className='bg-white'>
+         <div className='bg-white'> 
           <Header />
           <LoginForm />
-           </div>
+          
+          </div>
       </div>
     </div>
   );
