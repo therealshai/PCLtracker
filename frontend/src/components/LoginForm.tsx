7d3d04146274cd9b6a77b955d0e6f957bf5c7418
Syed Shaista Tabassum <sshai4020@gmail.com>
2024-06-21 09:16:53 +0530

    Fine tuning login page

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -6,15 +6,16 @@ const LoginForm: React.FC = () => {
       <form>
         <div className="mb-4">
           <label htmlFor="Student USN" className="block text-gray-700">Student Code</label>
-          <input type="text" id="studentCode" placeholder="Enter Student USM" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+          <input type="text" id="studentCode" placeholder="Enter Student USN" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
         </div>
         <div className="mb-6">
           <label htmlFor="password" className="block text-gray-700">Password</label>
           <input type="password" id="password" placeholder="password" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
         </div>
-        <button type="submit" className="w-full py-2 px-4 bg-indigo-600 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">LOG IN</button>
+      
+        <button type="submit" className="w-full py-2 px-4 bg-button-blue text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">LOG IN</button>
         <div className="mt-4 text-center">
-          <a href="#" className="text-indigo-600 hover:text-indigo-500">Forgot your password?</a>
+          {/* <a href="#" className="text-indigo-600 hover:text-indigo-500">Forgot your password?</a> */}
         </div>
       </form>
     </div>
